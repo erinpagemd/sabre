6e25fcedc409e449f7f6ca3606dc2f15fdd6cd73
YOUR NAME <erinpagemd@yahoo.com>
2015-03-12 22:06:46 -0500

    setting up views for home and login pages

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-//some js code probably declaring module, config?
+angular
+.module('anyPlane', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {url:'/', templateUrl:'views/home.html'})
+    .state('login', {url:'/login', templateUrl:'views/login.html'})
+    .state('results', {url:'/results', templateUrl:'views/results.html'});
+
+});
