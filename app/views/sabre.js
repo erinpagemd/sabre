68f09317f96ec6b197bdb91a55e9406ce446d9a3
YOUR NAME <erinpagemd@yahoo.com>
2015-03-16 15:57:38 -0500

    attempting to make a get call to sabre

--- app/views/sabre.js
+++ app/views/sabre.js
@@ -2,16 +2,29 @@ angular
 .module('anyPlane')
 .controller('SabreCtrl', SabreController)
 
-function SabreController ($scope) {
-  console.log('sabre!');
+function SabreController ($scope, $http) {
+  $scope.getTripData = function () {
+    $http(getReq($scope))
+    .success(function(data){
+      console.log('success with payload: ', data);
+    })
+    .error(function(data){
+      console.log('error retrieving data', data);
+    })
+  }
 };
+
+function createResults (obj) {
+  var fares = obj.FareInfo
+  console.log(fares);
+}
 
 function getUrl ($scope) {
   //store the values
   var origin = $scope.origin;
   var theme = $scope.theme;
-  var fare = $scope.fare;
-  var stay = $scope.stay;
+  var fare = ($scope.fare * 1);
+  var stay = ($scope.stay * 1);
   var startDate = $scope.date;
   //clear the inputs
   $scope.origin = null;
@@ -20,7 +33,7 @@ function getUrl ($scope) {
   $scope.stay = null;
   $scope.date = null;
 
-  var sabreUrl = 'https://api.text.sabre.com/vi/shop/flights/fares?origin=' + origin
+  var sabreUrl = 'https://api.test.sabre.com/v1/shop/flights/fares?origin=' + origin
                   + '&earliestdeparturedate=' + startDate
                   + '&latestdeparturedate=' + startDate
                   + '&lengthofstay=' + stay
@@ -28,6 +41,19 @@ function getUrl ($scope) {
                   + '&maxfare=' + fare
                   + '&pointofsalecountry=US&topdestinations=50';
 
-
-  access token Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTD!ICESMSLB\/CRT.LB!-3555685385332533852!1802243!0!!E2E-1
+  return sabreUrl;
 }
+
+function getReq ($scope) {
+  var token = 'Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/CERTG!ICESMSLB\/CRT.LB!-3555632266113144284!1197302!0!!E2E-1';
+  console.log(getUrl($scope));
+  var req = {
+    method: 'GET',
+    url: 'https://api.test.sabre.com/v1/shop/flights/fares?origin=ATL&earliestdeparturedate=2015-04-05&latestdeparturedate=2015-04-10&lengthofstay=2&theme=BEACH&maxfare=1000&pointofsalecountry=US&topdestinations=50',
+    headers: {
+      'Authorization': 'Bearer' + token
+    }
+  }
+
+  return req;
+};
